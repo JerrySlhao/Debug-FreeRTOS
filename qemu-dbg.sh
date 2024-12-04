e498707f8ababe22e57636af63df3df370bec534
shaoluhao <shaoluhao@xiaomi.com>
2024-12-04 17:13:43 +0800

    [Modify]: 修改VsCode调试文件

--- qemu-dbg.sh
+++ qemu-dbg.sh
@@ -1,1 +1,13 @@
-qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/build/gcc/output/RTOSDemo.out -serial mon:stdio -nographic -S -s
+#!/bin/bash
+
+#当前目录
+echo "Current directory: $(pwd)"
+
+# 获取当前脚本所在的目录
+SCRIPT_DIR=$(dirname "$0")
+
+# 切换到MPS
+cd ${SCRIPT_DIR}/FreeRTOS/FreeRTOS/Demo/CORTEX_MPS2_QEMU_IAR_GCC/build/gcc
+
+# 执行 make
+make
